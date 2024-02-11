779f2d86ab2c14b21a7c95812890521fcdff0c3b
Ariba S <153156108+asiddiqi16@users.noreply.github.com>
2024-02-11 16:57:46 +1100

    Add script for date and city lookup

--- src/script.js
+++ src/script.js
@@ -1,1 +1,55 @@
-alert("hello");
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let day = date.getDay();
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let formattedDay = days[day];
+  return `${formattedDay} ${hours}:${minutes}`;
+}
+
+function search(event) {
+  event.preventDefault();
+  let searchInputElement = document.querySelector("#enter-city");
+  let cityElement = document.querySelector("#city-selected");
+  cityElement.innerHTML = searchInputElement.value;
+
+  let city = searchInputElement.value;
+
+  /*let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=${unit}`;
+  getTemperature(apiUrl);*/
+}
+
+function convertTempC(tempC) {}
+function convertTempF(tempF) {}
+
+let currentDateELement = document.querySelector("#current-date");
+let currentDate = new Date();
+
+currentDateELement.innerHTML = formatDate(currentDate);
+
+let searchForm = document.querySelector("#city-search");
+searchForm.addEventListener("submit", search);
+
+let tempUnitCelsiusElement = document.querySelector("#temp-units-C");
+let tempUnitFaranheitElement = document.querySelector("#temp-units-F");
+
+tempUnitCelsiusElement.addEventListener("click", convertTempC);
+tempUnitFaranheitElement.addEventListener("click", convertTempF);
