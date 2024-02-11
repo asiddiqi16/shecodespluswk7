e0a26d9d69e01b602d5831fb4d65cc093b3643b5
Ariba S <153156108+asiddiqi16@users.noreply.github.com>
2024-02-11 17:58:51 +1100

    Add script for converting to imperial units

--- src/script.js
+++ src/script.js
@@ -25,6 +25,46 @@ function formatDate(date) {
   return `${formattedDay} ${hours}:${minutes}`;
 }
 
+function convertTempC(event) {
+  let metricElement = document.querySelector("#temp-units-C");
+  metricElement.classList.add("boldtext");
+  let imperialElement = document.querySelector("#temp-units-F");
+  imperialElement.classList.remove("boldtext");
+  event.preventDefault();
+  let cityElement = document.querySelector("#city-selected");
+  let city = cityElement.innerHTML;
+  let unit = "metric";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=${unit}`;
+  getTemperature(apiUrl);
+}
+
+function convertTempF(event) {
+  let metricElement = document.querySelector("#temp-units-C");
+  metricElement.classList.remove("boldtext");
+  let imperialElement = document.querySelector("#temp-units-F");
+  imperialElement.classList.add("boldtext");
+  event.preventDefault();
+  let cityElement = document.querySelector("#city-selected");
+  let city = cityElement.innerHTML;
+  let unit = "imperial";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=${unit}`;
+  getTemperature(apiUrl);
+}
+
+function updateTemperature(response) {
+  let currentTemperature = Math.round(response.data.temperature.current);
+  let temperatureElement = document.querySelector("#current-temperature-value");
+  temperatureElement.innerHTML = currentTemperature;
+
+  let weatherIconUrl = response.data.condition.icon_url;
+  let weatherIconElement = document.querySelector("#current-temperature-icon");
+  weatherIconElement.innerHTML = `<img src = "${weatherIconUrl}" width=120px>`;
+}
+
+function getTemperature(apiUrl) {
+  axios.get(apiUrl).then(updateTemperature);
+}
+
 function search(event) {
   event.preventDefault();
   let searchInputElement = document.querySelector("#enter-city");
@@ -33,12 +73,9 @@ function search(event) {
 
   let city = searchInputElement.value;
 
-  /*let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=${unit}`;
-  getTemperature(apiUrl);*/
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=${unit}`;
+  getTemperature(apiUrl);
 }
-
-function convertTempC(tempC) {}
-function convertTempF(tempF) {}
 
 let currentDateELement = document.querySelector("#current-date");
 let currentDate = new Date();
@@ -53,3 +90,6 @@ let tempUnitFaranheitElement = document.querySelector("#temp-units-F");
 
 tempUnitCelsiusElement.addEventListener("click", convertTempC);
 tempUnitFaranheitElement.addEventListener("click", convertTempF);
+
+let apiKey = "483ecb596o30da81tf76d2a4bf19d4a6";
+let unit = "metric";
